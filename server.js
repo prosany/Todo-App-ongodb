cda646535a5ae6e4d5e007b74b1ec9e74f22f4e2
Mahabub Sunny <mahabubsany2@gmail.com>
2022-05-19 02:29:29 +0600

    Cors

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const cors = require("cors");
-const bodyParser = require("body-parser");
 require("dotenv").config();
 
 // Define Port Number
 const port = process.env.PORT || 8080;
 
 // Use Cors and bodyParser
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
